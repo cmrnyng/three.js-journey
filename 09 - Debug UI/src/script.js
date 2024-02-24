3f371a48108339e4c3ee98d3c0b97f22b1071e05
Cameron <cameron.kamil.young@gmail.com>
2024-02-24 11:36:35 +0000

    finished lesson

--- 09 - Debug UI/src/script.js
+++ 09 - Debug UI/src/script.js
@@ -1,27 +1,48 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import gsap from "gsap";
+import GUI from "lil-gui";
 
-/**
- * Base
- */
+// Debug GUI
+const gui = new GUI();
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Object
- */
+// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
 const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-/**
- * Sizes
- */
+// Object debug
+const parameters = {
+  spin: () => {
+    gsap.to(mesh.rotation, {
+      y: mesh.rotation.y + Math.PI * 2,
+      duration: 2,
+    });
+  },
+};
+
+const positionFolder = gui.addFolder("Position");
+const meshFolder = gui.addFolder("Mesh");
+const functionFolder = gui.addFolder("Functions");
+positionFolder
+  .add(mesh.position, "y")
+  .min(-2)
+  .max(2)
+  .step(0.01)
+  .name("Elevation");
+meshFolder.add(mesh, "visible");
+meshFolder.add(material, "wireframe");
+meshFolder.addColor(material, "color");
+functionFolder.add(parameters, "spin").name("Spin the cube!");
+
+// Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -41,10 +62,7 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-/**
- * Camera
- */
-// Base camera
+// Camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
@@ -60,18 +78,14 @@ scene.add(camera);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock();
 
 const tick = () => {
