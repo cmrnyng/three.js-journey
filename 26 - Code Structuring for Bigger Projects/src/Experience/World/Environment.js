bbf97ba4bec90615a2f27237abd47089634e0daf
Cameron Young <cameron.kamil.young@gmail.com>
2024-03-10 16:42:13 +0000

    finished lesson

--- 26 - Code Structuring for Bigger Projects/src/Experience/World/Environment.js
+++ 26 - Code Structuring for Bigger Projects/src/Experience/World/Environment.js
@@ -6,6 +6,12 @@ export default class Environment {
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
+    this.debug = this.experience.debug;
+
+    // Debug
+    if (this.debug.active) {
+      this.debugFolder = this.debug.ui.addFolder("environment");
+    }
 
     // Setup
     this.setSunLight();
@@ -20,6 +26,25 @@ export default class Environment {
     this.sunLight.shadow.normalBias = 0.05;
     this.sunLight.position.set(3.5, 2, -1.25);
     this.scene.add(this.sunLight);
+
+    // Debug
+    if (this.debug.active) {
+      this.debugFolder
+        .add(this.sunLight, "intensity", 0, 10, 0.001)
+        .name("sunLightIntensity");
+
+      this.debugFolder
+        .add(this.sunLight.position, "x", -5, 5, 0.001)
+        .name("sunLightX");
+
+      this.debugFolder
+        .add(this.sunLight.position, "y", -5, 5, 0.001)
+        .name("sunLightY");
+
+      this.debugFolder
+        .add(this.sunLight.position, "z", -5, 5, 0.001)
+        .name("sunLightZ");
+    }
   }
 
   setEnvironmentMap() {
@@ -43,5 +68,13 @@ export default class Environment {
       });
     };
     this.environmentMap.updateMaterials();
+
+    // Debug
+    if (this.debug.active) {
+      this.debugFolder
+        .add(this.environmentMap, "intensity", 0, 4, 0.001)
+        .name("envMapIntensity")
+        .onChange(this.environmentMap.updateMaterials);
+    }
   }
 }
--- 26 - Code Structuring for Bigger Projects/src/Experience/World/World.js
+++ 26 - Code Structuring for Bigger Projects/src/Experience/World/World.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import Experience from "../Experience.js";
 import Environment from "./Environment.js";
+import Floor from "./Floor.js";
+import Fox from "./Fox.js";
 
 export default class World {
   constructor() {
@@ -8,17 +10,16 @@ export default class World {
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
 
-    // Test mesh
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial()
-    );
-    this.scene.add(testMesh);
-
     // Wait for resources
     this.resources.on("ready", () => {
       // Setup
+      this.fox = new Fox();
+      this.floor = new Floor();
       this.environment = new Environment();
     });
+  }
+
+  update() {
+    this.fox && this.fox.update();
   }
 }
