31323fef9ea7ed1287a2ce433d49e40d36925c82
Cameron Young <cameron.kamil.young@gmail.com>
2024-03-18 13:51:32 +0000

    finished lesson

--- 43 - R3F and Drei/src/Experience.jsx
+++ 43 - R3F and Drei/src/Experience.jsx
@@ -1,32 +1,76 @@
-import { useThree, extend } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-extend({ OrbitControls });
+import {
+  OrbitControls,
+  TransformControls,
+  PivotControls,
+  Html,
+  Text,
+  Float,
+  MeshReflectorMaterial,
+} from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Experience() {
-  const { camera, gl } = useThree();
+  const cube = useRef();
+  const sphere = useRef();
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+        scale={1.5}
+      >
+        <mesh ref={sphere} position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+          <Html
+            wrapperClass="label"
+            position={[1, 1, 0]}
+            distanceFactor={7}
+            center
+            occlude={[sphere, cube]}
+          >
+            This is a sphere 😶
+          </Html>
+        </mesh>
+      </PivotControls>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
+      <TransformControls object={cube} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <MeshReflectorMaterial
+          resolution={512}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.75}
+          color="greenyellow"
+        />
       </mesh>
+
+      <Float speed={5} floatIntensity={2}>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          fontSize={1}
+          color="salmon"
+          position-y={2}
+          maxWidth={3}
+          textAlign="center"
+        >
+          R3F and Drei
+        </Text>
+      </Float>
     </>
   );
 }
