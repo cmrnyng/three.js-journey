3efac0059cc7dfd063da6a8966f6ce68ad677b22
Cameron Young <cameron.kamil.young@gmail.com>
2024-03-22 12:00:07 +0000

    finished lesson

--- 47 - 3D Text with R3F/src/Experience.jsx
+++ 47 - 3D Text with R3F/src/Experience.jsx
@@ -1,17 +1,74 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Text3D, Center, useMatcapTexture } from "@react-three/drei";
+import { useState, useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 import { Perf } from "r3f-perf";
 
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
+
 export default function Experience() {
+  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
+  const donuts = useRef([]);
+
+  useEffect(() => {
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
+
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, []);
+
+  // const [torusGeometry, setTorusGeometry] = useState();
+  // const [material, setMaterial] = useState();
+
+  useFrame((_state, dt) => {
+    for (const donut of donuts.current) {
+      donut.rotation.y += dt * 0.2;
+    }
+  });
+
   return (
     <>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
-      <mesh scale={1.5}>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      {/* <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} /> */}
+
+      <Center>
+        <Text3D
+          font="./fonts/helvetiker_regular.typeface.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+          material={material}
+        >
+          HELLO R3F
+        </Text3D>
+      </Center>
+
+      {[...Array(100)].map((_el, i) => (
+        <mesh
+          ref={el => (donuts.current[i] = el)}
+          key={i}
+          geometry={torusGeometry}
+          material={material}
+          position={[
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+          ]}
+          scale={0.2 + Math.random() * 0.2}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+        />
+      ))}
     </>
   );
 }
